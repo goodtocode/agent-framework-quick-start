1d20c91b34775a49fdc87b5b4e4e548d0ed234c5
Robert Good <robert.good@goodtocode.com>
2026-02-01 21:53:04 -0800

    Specs to Tests

---
+++ src/Tests.Integration/ChatCompletion/DeleteChatSessionCommandStepDefinitions.cs
@@ -0,0 +1,76 @@
+using Goodtocode.AgentFramework.Core.Application.ChatCompletion;
+using Goodtocode.AgentFramework.Core.Domain.ChatCompletion;
+
+namespace Goodtocode.AgentFramework.Tests.Integration.ChatCompletion
+{
+    [Binding]
+    [Scope(Tag = "deleteChatSessionCommand")]
+    public class DeleteChatSessionCommandStepDefinitions : TestBase
+    {
+        private Guid _id;
+        private bool _exists;
+
+        [Given(@"I have a def ""([^""]*)""")]
+        public void GivenIHaveADef(string def)
+        {
+            base.def = def;
+        }
+
+        [Given(@"I have a chat session id""([^""]*)""")]
+        public void GivenIHaveAChatSessionKey(string id)
+        {
+            Guid.TryParse(id, out _id).ShouldBeTrue();
+        }
+
+        [Given(@"The chat session exists ""([^""]*)""")]
+        public void GivenTheChatSessionExists(string exists)
+        {
+            bool.TryParse(exists, out _exists).ShouldBeTrue();
+        }
+
+        [When(@"I delete the chat session")]
+        public async Task WhenIDeleteTheChatSession()
+        {
+            var request = new DeleteChatSessionCommand()
+            {
+                Id = _id
+            };
+
+            if (_exists)
+            {
+                var chatSession = ChatSessionEntity.Create(_id, Guid.NewGuid(), "Test Session", ChatMessageRole.assistant, "First Message", "First Response");
+                context.ChatSessions.Add(chatSession);
+                await context.SaveChangesAsync(CancellationToken.None);
+            }
+
+            var validator = new DeleteChatSessionCommandValidator();
+            validationResponse = await validator.ValidateAsync(request);
+
+            if (validationResponse.IsValid)
+                try
+                {
+                    var handler = new DeleteChatSessionCommandHandler(context);
+                    await handler.Handle(request, CancellationToken.None);
+                    responseType = CommandResponseType.Successful;
+                }
+                catch (Exception e)
+                {
+                    HandleAssignResponseType(e);
+                }
+            else
+                responseType = CommandResponseType.BadRequest;
+        }
+
+        [Then(@"The response is ""([^""]*)""")]
+        public void ThenTheResponseIs(string response)
+        {
+            HandleHasResponseType(response);
+        }
+
+        [Then(@"If the response has validation issues I see the ""([^""]*)"" in the response")]
+        public void ThenIfTheResponseHasValidationIssuesISeeTheInTheResponse(string expectedErrors)
+        {
+            HandleExpectedValidationErrorsAssertions(expectedErrors);
+        }
+    }
+}
---
+++ src/Tests.Integration/ChatCompletion/GetChatSessionsQueryStepDefinitions.cs
@@ -0,0 +1,119 @@
+using Goodtocode.AgentFramework.Core.Domain.ChatCompletion;
+using Goodtocode.AgentFramework.Core.Application.ChatCompletion;
+
+namespace Goodtocode.AgentFramework.Tests.Integration.ChatCompletion;
+
+[Binding]
+[Scope(Tag = "getChatSessionsQuery")]
+public class GetChatSessionsQueryStepDefinitions : TestBase
+{
+    private bool _exists;
+    private bool _withinDateRangeExists;
+    private DateTime _endDate;
+    private DateTime _startDate;
+    private ICollection<ChatSessionDto>? _response;
+
+    [Given(@"I have a definition ""([^""]*)""")]
+    public void GivenIHaveADefinition(string def)
+    {
+        base.def = def;
+    }
+
+    [Given(@"Chat Sessions exist ""([^""]*)""")]
+    public void GivenChatSessionsExist(string exists)
+    {
+        bool.TryParse(exists, out _exists).ShouldBeTrue();
+    }
+
+    [Given(@"chat sessions within the date range exists ""([^""]*)""")]
+    public void GivenChatSessionsWithinTheDateRangeExists(string withinDateRangeExists)
+    {
+        bool.TryParse(withinDateRangeExists, out _withinDateRangeExists).ShouldBeTrue();
+    }
+
+    [Given(@"I have a start date ""([^""]*)""")]
+    public void GivenIHaveAStartDate(string startDate)
+    {
+        if (string.IsNullOrWhiteSpace(startDate)) return;
+        DateTime.TryParse(startDate, out _startDate).ShouldBeTrue();
+        _startDate = DateTime.UtcNow.AddMinutes(_withinDateRangeExists ? -1 : 1); //Handle for desired not-found scenarios
+    }
+
+    [Given(@"I have a end date ""([^""]*)""")]
+    public void GivenIHaveAEndDate(string endDate)
+    {
+        if (string.IsNullOrWhiteSpace(endDate)) return;
+        DateTime.TryParse(endDate, out _endDate).ShouldBeTrue();
+    }
+
+    [When(@"I get the chat sessions")]
+    public async Task WhenIGetTheChatSessions()
+    {
+        if (_exists)
+        {
+            var chatSession = ChatSessionEntity.Create(Guid.NewGuid(), Guid.NewGuid(), "Test Session", ChatMessageRole.assistant, "First Message", "First Response");
+            context.ChatSessions.Add(chatSession);
+            await context.SaveChangesAsync(CancellationToken.None);
+        }
+
+        var request = new GetChatSessionsQuery()
+        {
+            StartDate = _startDate == default ? null : _startDate,
+            EndDate = _endDate == default ? null : _endDate
+        };
+
+        var validator = new GetChatSessionsQueryValidator();
+        validationResponse = validator.Validate(request);
+        if (validationResponse.IsValid)
+            try
+            {
+                var handler = new GetChatSessionsQueryHandler(context);
+                _response = await handler.Handle(request, CancellationToken.None);
+                responseType = CommandResponseType.Successful;
+            }
+            catch (Exception e)
+            {
+                responseType = HandleAssignResponseType(e);
+            }
+        else
+            responseType = CommandResponseType.BadRequest;
+    }
+
+    [Then(@"The response is ""([^""]*)""")]
+    public void ThenTheResponseIs(string response)
+    {
+        HandleHasResponseType(response);
+    }
+
+    [Then(@"If the response has validation issues I see the ""([^""]*)"" in the response")]
+    public void ThenIfTheResponseHasValidationIssuesISeeTheInTheResponse(string expectedErrors)
+    {
+        HandleExpectedValidationErrorsAssertions(expectedErrors);
+    }
+
+    [Then(@"The response has a collection of chat sessions")]
+    public void ThenTheResponseHasACollectionOfChatSessions()
+    {
+        _response?.Count.ShouldBe(_withinDateRangeExists == false ? 0 : _response.Count);
+    }
+
+    [Then(@"Each chat session has a Key")]
+    public void ThenEachChatSessionHasAKey()
+    {
+        _response?.FirstOrDefault(x => x.Id == default).ShouldBeNull();
+    }
+
+    [Then(@"Each chat session has a Date greater than start date")]
+    public void ThenEachChatSessionHasADateGreaterThanStartDate()
+    {
+        if (_withinDateRangeExists)
+            _response?.FirstOrDefault(x => _startDate == default || x.Timestamp > _startDate).ShouldNotBeNull();
+    }
+
+    [Then(@"Each chat session has a Date less than end date")]
+    public void ThenEachChatSessionHasADateLessThanEndDate()
+    {
+        if (_withinDateRangeExists)
+            _response?.FirstOrDefault(x => _endDate == default || x.Timestamp < _endDate).ShouldNotBeNull();
+    }
+}
