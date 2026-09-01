4057a441fa535b4547db31cad25626ee3ad680d4
Robert J. Good <robert.good@goodtocode.com>
2026-09-01 16:51:22 -0700

    removed dups

--- src/Core.Application/Abstractions/IChatMessagesTool.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Goodtocode.AgentFramework.Core.Application.Abstractions;
-
-public interface IMyChatMessagesTool
-{
-    Task<IEnumerable<string>> ListRecentMessagesAsync(DateTime? startDate, DateTime? endDate, CancellationToken cancellationToken);
-    Task<IEnumerable<string>> GetChatMessagesAsync(Guid sessionId, CancellationToken cancellationToken);
-}
--- src/Core.Application/Abstractions/IChatSessionsTool.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Goodtocode.AgentFramework.Core.Application.Abstractions;
-
-public interface IMyChatSessionsTool
-{
-    Task<IEnumerable<string>> ListRecentSessionsAsync(DateTime? startDate, DateTime? endDate, CancellationToken cancellationToken);
-    Task<string?> UpdateChatSessionTitleAsync(Guid sessionId, string newTitle, CancellationToken cancellationToken);
-}
